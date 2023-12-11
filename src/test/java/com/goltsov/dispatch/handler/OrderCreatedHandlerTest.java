1fbbb7c0a55c233f6dafd3f23d9a5272d697f4db
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-12-11 22:47:08 +0100

    feat: add listenService failure handling test

--- src/test/java/com/goltsov/dispatch/handler/OrderCreatedHandlerTest.java
+++ src/test/java/com/goltsov/dispatch/handler/OrderCreatedHandlerTest.java
@@ -6,12 +6,13 @@ import com.goltsov.dispatch.service.DispatchService;
 import com.goltsov.dispatch.util.TestEventData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static java.util.UUID.randomUUID;
 
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class OrderCreatedHandlerTest {
 
@@ -26,9 +27,19 @@ class OrderCreatedHandlerTest {
     }
 
     @Test
-    void listen() {
+    void listenSuccess() throws Exception {
         OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
         handler.listen(testEvent);
-        Mockito.verify(dispatchServiceMock, times(1)).process(testEvent);
+        verify(dispatchServiceMock, times(1)).process(testEvent);
+    }
+
+    @Test
+    public void listenServiceThrowsException() throws Exception {
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        doThrow(new RuntimeException("Service failure")).when(dispatchServiceMock).process(testEvent);
+
+        handler.listen(testEvent);
+
+        verify(dispatchServiceMock, times(1)).process(testEvent);
     }
 }
