dd27e00162ed94bd044984e832474d8be4c22d64
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-12-11 22:45:30 +0100

    feat: add functional process method. Class is now injected with KafkaTemplate
    The commit will also remove unused imports

--- src/main/java/com/goltsov/dispatch/service/DispatchService.java
+++ src/main/java/com/goltsov/dispatch/service/DispatchService.java
@@ -1,12 +1,29 @@
 package com.goltsov.dispatch.service;
 
 import com.goltsov.dispatch.message.OrderCreated;
+import com.goltsov.dispatch.message.OrderDispatched;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutionException;
+
 @Service
+@RequiredArgsConstructor
 public class DispatchService {
 
-    public void process(OrderCreated payload) {
-        // To do
+    private static final String ORDER_DISPATCHER_TOPIC = "order.dispatched";
+
+    private final KafkaTemplate<String, Object> kafkaProducer;
+
+    public void process(OrderCreated orderCreated) throws Exception {
+        OrderDispatched orderDispatched = OrderDispatched.builder()
+                .orderId(orderCreated.getOrderId())
+                .build();
+
+        kafkaProducer.send(ORDER_DISPATCHER_TOPIC, orderDispatched).get();
     }
 }
