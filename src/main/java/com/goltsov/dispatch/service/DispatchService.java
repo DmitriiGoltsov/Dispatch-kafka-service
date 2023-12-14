f7d2e6fce38f1d9ac1a75195b97083816fae2f8d
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-12-14 17:19:05 +0100

    fix: add tests for DispatchPreparing entities and for a new functionality of DispatchService

--- src/main/java/com/goltsov/dispatch/service/DispatchService.java
+++ src/main/java/com/goltsov/dispatch/service/DispatchService.java
@@ -1,29 +1,36 @@
 package com.goltsov.dispatch.service;
 
+import com.goltsov.dispatch.message.DispatchPreparing;
 import com.goltsov.dispatch.message.OrderCreated;
 import com.goltsov.dispatch.message.OrderDispatched;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ExecutionException;
-
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class DispatchService {
 
-    private static final String ORDER_DISPATCHER_TOPIC = "order.dispatched";
+    private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
+
+    private static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
 
     private final KafkaTemplate<String, Object> kafkaProducer;
 
     public void process(OrderCreated orderCreated) throws Exception {
+
+        DispatchPreparing dispatchPreparing = DispatchPreparing.builder()
+                .orderId(orderCreated.getOrderId())
+                .build();
+        kafkaProducer.send(DISPATCH_TRACKING_TOPIC, dispatchPreparing).get();
+
         OrderDispatched orderDispatched = OrderDispatched.builder()
                 .orderId(orderCreated.getOrderId())
                 .build();
-
-        kafkaProducer.send(ORDER_DISPATCHER_TOPIC, orderDispatched).get();
+        kafkaProducer.send(ORDER_DISPATCHED_TOPIC, orderDispatched).get();
     }
 }
