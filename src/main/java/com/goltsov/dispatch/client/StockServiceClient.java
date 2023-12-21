2dd62b719beeca457c9566b9cc00b3345a84116d
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-12-21 18:44:23 +0100

    feat: add a client class for checking a stock service class

---
+++ src/main/java/com/goltsov/dispatch/client/StockServiceClient.java
@@ -0,0 +1,43 @@
+package com.goltsov.dispatch.client;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@Slf4j
+public class StockServiceClient {
+
+    private final RestTemplate restTemplate;
+
+    private final String stockServiceEndpoint;
+
+    public StockServiceClient(@Autowired RestTemplate restTemplate,
+                              @Value("$dispatch.stockServiceEndpoint") String stockServiceEndpoint) {
+        this.restTemplate = restTemplate;
+        this.stockServiceEndpoint = stockServiceEndpoint;
+    }
+
+    public String checkAvailability(String item) {
+
+        try {
+            ResponseEntity<String> response = restTemplate
+                    .getForEntity(stockServiceEndpoint + "?item=" + item, String.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                throw new RuntimeException("error " + response.getStatusCode());
+            }
+            return response.getBody();
+        } catch (HttpServerErrorException | ResourceAccessException exception) {
+            log.warn("Failure calling external service", exception);
+            throw exception;
+        }
+    }
+
+
+}
