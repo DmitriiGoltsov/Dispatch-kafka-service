f7e31d5fd82409f3958f512b1f0e12b03558770a
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-12-22 14:04:40 +0100

    feat: add catches clauses for situations when retryable and not retryable exceptions are thrown

--- src/main/java/com/goltsov/dispatch/handler/OrderCreatedHandler.java
+++ src/main/java/com/goltsov/dispatch/handler/OrderCreatedHandler.java
@@ -1,9 +1,13 @@
 package com.goltsov.dispatch.handler;
 
+import com.goltsov.dispatch.exception.NotRetryableException;
+import com.goltsov.dispatch.exception.RetryableException;
 import com.goltsov.dispatch.message.OrderCreated;
 import com.goltsov.dispatch.service.DispatchService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -11,8 +15,8 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
 @RequiredArgsConstructor
+@Component
 public class OrderCreatedHandler {
 
     private final DispatchService dispatchService;
@@ -23,15 +27,16 @@ public class OrderCreatedHandler {
             groupId = "dispatch.order.created.consumer",
             containerFactory = "kafkaListenerContainerFactory"
     )
-    public void listen(@Header(KafkaHeaders.RECEIVED_PARTITION) Integer partition,
-                       @Header(KafkaHeaders.RECEIVED_KEY) String key,
-                       @Payload OrderCreated payload) {
-        log.info("Received a message: partition: " + partition + ". Key: " + key + ". The payload is: " + payload);
-
+    public void listen(@Header(KafkaHeaders.RECEIVED_PARTITION) Integer partition, @Header(KafkaHeaders.RECEIVED_KEY) String key, @Payload OrderCreated payload) {
+        log.info("Received message: partition: "+partition+" - key: " +key+ " - orderId: " + payload.getOrderId() + " - item: " + payload.getItem());
         try {
             dispatchService.process(key, payload);
+        } catch (RetryableException e) {
+            log.warn("Retryable exception: " + e.getMessage());
+            throw e;
         } catch (Exception e) {
-            log.error("Processing failure", e);
+            log.error("NotRetryable exception: " + e.getMessage());
+            throw new NotRetryableException(e);
         }
     }
 }
