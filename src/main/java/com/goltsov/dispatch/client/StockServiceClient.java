cd47aed8e75ab9820dd7b140cd0b8974329cd5f9
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-12-22 14:07:35 +0100

    feat: add a service to check if a good is available on the stock.

--- src/main/java/com/goltsov/dispatch/client/StockServiceClient.java
+++ src/main/java/com/goltsov/dispatch/client/StockServiceClient.java
@@ -1,5 +1,6 @@
 package com.goltsov.dispatch.client;
 
+import com.goltsov.dispatch.exception.RetryableException;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,37 +8,39 @@ import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
-@Component
 @Slf4j
+@Component
 public class StockServiceClient {
 
     private final RestTemplate restTemplate;
 
     private final String stockServiceEndpoint;
 
-    public StockServiceClient(@Autowired RestTemplate restTemplate,
-                              @Value("${dispatch.stockServiceEndpoint}") String stockServiceEndpoint) {
+    public StockServiceClient(@Autowired RestTemplate restTemplate, @Value("${dispatch.stockServiceEndpoint}") String stockServiceEndpoint) {
         this.restTemplate = restTemplate;
         this.stockServiceEndpoint = stockServiceEndpoint;
     }
 
+    /**
+     * The stock service returns true if item is available, false otherwise.
+     */
     public String checkAvailability(String item) {
-
         try {
-            ResponseEntity<String> response = restTemplate
-                    .getForEntity(stockServiceEndpoint + "?item=" + item, String.class);
+            ResponseEntity<String> response = restTemplate.getForEntity(stockServiceEndpoint+"?item="+item, String.class);
             if (!response.getStatusCode().is2xxSuccessful()) {
-                throw new RuntimeException("error " + response.getStatusCode());
+                throw new RuntimeException("error " + response.getStatusCode().value());
             }
             return response.getBody();
-        } catch (HttpServerErrorException | ResourceAccessException exception) {
-            log.warn("Failure calling external service", exception);
-            throw exception;
+        } catch (HttpServerErrorException | ResourceAccessException e) {
+            log.warn("Failure calling external service", e);
+            throw new RetryableException(e);
+        } catch (Exception e) {
+            log.error("Exception thrown: " + e.getClass().getName(), e);
+            throw e;
         }
     }
 }
